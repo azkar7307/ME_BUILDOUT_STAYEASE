3054939f64441057a09a06b7056d7b7eec17506c
CrioUser <crio-user@criodo.com>
2025-11-29 17:54:55 +0530

    feat: add login request dto

---
+++ src/main/java/com/takehome/stayease/dto/request/LoginRequest.java
@@ -0,0 +1,27 @@
+package com.takehome.stayease.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+    
+    @NotBlank(message = "Password must not be empty")
+    @Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!_]).{8,}$",
+        message = "Password must be at least 8 characters long, contain one uppercase letter,"
+            + "one number, and one special character"
+    )
+    private String password;
+}
