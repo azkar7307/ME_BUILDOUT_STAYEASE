65b4e1e432a19b33e2ae4a91d0d216fa06c14da2
CrioUser <crio-user@criodo.com>
2025-11-29 22:27:58 +0530

    feat: add user registeration and login endpoint

---
+++ src/main/java/com/takehome/stayease/controller/AuthController.java
@@ -0,0 +1,47 @@
+package com.takehome.stayease.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import com.takehome.stayease.dto.request.LoginRequest;
+import com.takehome.stayease.dto.request.RegisterRequest;
+import com.takehome.stayease.dto.response.AuthResponse;
+import com.takehome.stayease.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> registerUser(
+        @RequestBody @Valid RegisterRequest registerRequest
+    ) {
+        log.info(
+            "Request received to register new user with email '{}'", 
+            registerRequest.getEmail()
+        );
+        return new ResponseEntity<>(
+            authService.registerUser(registerRequest), 
+            HttpStatus.OK
+        );
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> loginUser(@RequestBody @Valid LoginRequest loginRequest) {
+        log.info("Request received to login user with email {}", loginRequest.getEmail());
+        return new ResponseEntity<>(
+            authService.loginUser(loginRequest),
+            HttpStatus.OK
+        );
+    }
+    
+}
