677a138e1d43a5b02d4310a071c7c60b4c8dd89b
CrioUser <crio-user@criodo.com>
2025-11-29 18:09:52 +0530

    feat: add authenticated response dto

---
+++ src/main/java/com/takehome/stayease/dto/response/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.takehome.stayease.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+    private final String jwtToken;
+}
