42cc2879e8650e196d646cc281bc255e3baf9b7d
CrioUser <crio-user@criodo.com>
2025-11-30 22:54:53 +0530

    refactor: customize ModelMapper for BookingResponse DTO

--- src/main/java/com/takehome/stayease/config/AppConfig.java
+++ src/main/java/com/takehome/stayease/config/AppConfig.java
@@ -1,6 +1,9 @@
 package com.takehome.stayease.config;
 
+import com.takehome.stayease.dto.response.BookingResponse;
+import com.takehome.stayease.entity.Booking;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,22 @@ public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper =  new ModelMapper();
+
+        TypeMap<Booking, BookingResponse> bookingMap = modelMapper.createTypeMap(
+            Booking.class, 
+            BookingResponse.class
+        );
+
+        bookingMap.addMappings(mapper -> {
+            mapper.map(Booking::getId, BookingResponse :: setBookingId);
+            mapper.map(src -> src.getHotel().getId(), BookingResponse::setHotelId);
+        }); 
+
+        // bookingMap.addMappings(mapper -> {
+        //     mapper.map(src -> src.getId(), (dest, value) -> dest.setBookingId((Long) value));
+        //     mapper.map(src -> src.getHotel().getId(), (dest, value) -> dest.setHotelId((Long) value));
+        // }); 
+        return modelMapper;
     }
 }
