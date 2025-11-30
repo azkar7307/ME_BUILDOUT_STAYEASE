c8dfe2dccf2ea93f17d94df867146ddf1d21c10c
CrioUser <crio-user@criodo.com>
2025-11-30 18:09:40 +0530

    feat: add booking entity with its repository

---
+++ src/main/java/com/takehome/stayease/entity/Booking.java
@@ -0,0 +1,34 @@
+package com.takehome.stayease.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "bookings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    Long id;
+    
+    @Column(nullable = false)
+    LocalDate checkInDate;
+
+    @Column(nullable = false)
+    LocalDate checkOutDate;
+}
---
+++ src/main/java/com/takehome/stayease/repository/BookingRepository.java
@@ -0,0 +1,8 @@
+package com.takehome.stayease.repository;
+
+import com.takehome.stayease.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    
+}
