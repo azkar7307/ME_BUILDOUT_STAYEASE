71bb4613e11183e6e086c7cd711c01d3e1551326
CrioUser <crio-user@criodo.com>
2025-11-30 15:51:27 +0530

    test: add context load test for application startup

---
+++ src/test/java/com/takehome/stayease/StayeaseApplicationTest.java
@@ -0,0 +1,13 @@
+package com.takehome.stayease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StayeaseApplicationTest {
+
+    @Test
+	void contextLoads() {
+	}
+
+}
