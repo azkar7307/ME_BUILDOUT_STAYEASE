5b03f90e1d007c48ab5400d5a518ad3a2f91f681
CrioUser <crio-user@criodo.com>
2025-11-30 22:45:20 +0530

    refactor: add boilerplate code using lombok annotations in BookingRequest DTO

--- src/main/java/com/takehome/stayease/dto/request/BookingRequest.java
+++ src/main/java/com/takehome/stayease/dto/request/BookingRequest.java
@@ -1,8 +1,16 @@
 package com.takehome.stayease.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingRequest {
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate checkInDate;
