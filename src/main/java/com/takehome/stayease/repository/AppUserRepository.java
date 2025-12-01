479cf425b7374c3a8f6384f1ee4b4c911f325a69
CrioUser <crio-user@criodo.com>
2025-12-02 01:25:47 +0530

    feat: add validation functionality in validation service

--- src/main/java/com/takehome/stayease/repository/AppUserRepository.java
+++ src/main/java/com/takehome/stayease/repository/AppUserRepository.java
@@ -1,9 +1,14 @@
 package com.takehome.stayease.repository;
 
+import java.util.Optional;
 import com.takehome.stayease.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    boolean existByEmail();
+
+    Optional<AppUser> findByEmail(String email);
 }
---
+++ src/main/java/com/takehome/stayease/service/impl/ValidationServiceImpl.java
@@ -0,0 +1,98 @@
+package com.takehome.stayease.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import java.time.LocalDate;
+import com.takehome.stayease.dto.request.BookingRequest;
+import com.takehome.stayease.entity.AppUser;
+import com.takehome.stayease.entity.Booking;
+import com.takehome.stayease.entity.Hotel;
+import com.takehome.stayease.exception.BadRequestException;
+import com.takehome.stayease.exception.EntityNotFoundException;
+import com.takehome.stayease.repository.AppUserRepository;
+import com.takehome.stayease.repository.BookingRepository;
+import com.takehome.stayease.repository.HotelRepository;
+import com.takehome.stayease.service.ValidationService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ValidationServiceImpl implements ValidationService {
+    private final AppUserRepository userRepository;
+    private final HotelRepository hotelRepository;
+    private final BookingRepository bookingRepository;
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public void ValidateUserExistByEmail(String email) {
+        if (userRepository.existByEmail()) {
+            throw new BadRequestException("User with email '" + email + "' already exists");
+        }
+        
+    }
+
+    @Override
+    public AppUser validateAndGetUserByEmail(String email) {
+        AppUser user = userRepository.findByEmail(email).orElseThrow(
+            () -> new EntityNotFoundException(email, "User")
+        );
+        return user;
+    }
+
+    @Override
+    public Hotel validateAndGetHotel(Long id) {
+        Hotel hotel = hotelRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException(id, "User")
+        );
+        return hotel;
+    }
+
+    @Override
+    public void validateBookingDates(BookingRequest bookingRequest) {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate checkInDate = bookingRequest.getCheckInDate();
+        LocalDate checkOutDate = bookingRequest.getCheckOutDate();
+        
+        if (checkInDate.equals(currentDate) || checkInDate.isBefore(currentDate)) {
+            throw new BadRequestException("Check-in-date must be future date");
+        }
+
+        if (checkInDate.isAfter(checkOutDate)) {
+            throw new BadRequestException("Check-in-date must be bofore check-out-date");
+        }
+    }
+
+    @Override
+    public void validateRoomAvailablity(Hotel hotel) {
+        if (hotel.getAvailableRooms() < 1) {
+            throw new BadRequestException("The requested hotel '" 
+                + hotel.getId() 
+                + "' doesn't have available room"
+            );
+        }
+    }
+
+    @Override
+    public Booking validateUserAndBooking(Long bookingId, Long userId) {
+        Booking booking = validateAndGetBooking(bookingId);
+        if (!booking.getUser().getId().equals(userId)) {
+            throw new BadRequestException("Customer '" 
+                + userId 
+                + " have not booked with Booking '" 
+                + bookingId
+                + "'"
+            );
+        }
+        return booking;
+    }
+
+    @Override
+    public Booking validateAndGetBooking(Long bookingId) {
+        Booking booking = bookingRepository.findById(bookingId).orElseThrow(
+            () -> new EntityNotFoundException(bookingId, "Booking")
+        );
+        return booking;
+    }
+    
+}
