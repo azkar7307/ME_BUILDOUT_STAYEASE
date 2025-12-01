cd6cca4c94d6c79ae963b26331aca1d5c1e9c4b0
CrioUser <crio-user@criodo.com>
2025-12-01 21:01:11 +0530

    feat: add error response dto

---
+++ src/main/java/com/takehome/stayease/dto/response/ErrorResponse.java
@@ -0,0 +1,24 @@
+package com.takehome.stayease.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private String httpStatus;
+    private LocalDateTime timestamp;   
+}
+
+
+
