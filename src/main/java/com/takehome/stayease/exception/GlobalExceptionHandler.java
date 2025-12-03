ed57ac9c96563d6c5572ca89da515efe75b9f30d
CrioUser <crio-user@criodo.com>
2025-12-03 14:50:41 +0530

    feat: handle BadCredentialsException with global exception handler for sign-in

--- src/main/java/com/takehome/stayease/exception/GlobalExceptionHandler.java
+++ src/main/java/com/takehome/stayease/exception/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,17 @@ public class GlobalExceptionHandler {
         HttpStatus status = HttpStatus.NOT_FOUND;
         ErrorResponse errorResponse = buildErrorResponse(ex.getMessage(), status, request);
         log.error("Resource Not Found - {}", errorResponse);
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredential(
+            BadCredentialsException ex,
+            WebRequest request
+    ) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse errorResponse = buildErrorResponse(ex.getMessage(), status, request);
+        log.error("Bad Credentials - {}", errorResponse);
         return ResponseEntity.status(status).body(errorResponse);
     }
 
