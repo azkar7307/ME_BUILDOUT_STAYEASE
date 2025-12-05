251184f09a26c01e8a25a163d53bc65525118a24
CrioUser <crio-user@criodo.com>
2025-12-05 16:34:49 +0530

    ME_BUILDOUT_STAYEASE_MODULE_L1

--- src/main/java/com/takehome/stayease/dto/request/RegisterRequest.java
+++ src/main/java/com/takehome/stayease/dto/request/RegisterRequest.java
@@ -2,7 +2,6 @@ package com.takehome.stayease.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
